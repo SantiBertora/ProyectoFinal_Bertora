5c478c2371be880d5c7006e9f5d8c82120ee0013
Santiago Bertora <santibertoratrujillo@gmail.com>
2023-02-18 18:00:17 -0300

    programa listo. Falta filtrado, libreria y ultimar estilos

--- js/main.js
+++ js/main.js
@@ -1,34 +1,142 @@
-//VARIABLES
+//FUNCIONES
 
-const carrito = [];
-let totalAPagar = 0;
+function editarCantidad (producto, nuevaCantidad) {
 
-//DOM
-
-const listadoProductos = document.getElementById("listadoProductos");
-const listadoCarrito = document.getElementById("listadoCarrito");
-const precioFinal = document.getElementById("precioFinal");
-
-//OBTENEMOS LOS PRODUCTOS JSON
-
-fetch("/productos.json")
-    .then((response) => {
-        return response.json();
-    })
-    .then((responseProductos) => {
-        renderizarProductos(responseProductos);
+    const indiceProductoExiste = carrito.findIndex( (productoCarrito) => {
+        return productoCarrito.nombre === producto.nombre;
     });
 
-//FUNCION RENDERIZAR PRODUCTOS
+    if(indiceProductoExiste !== -1) {
 
-/*  <li class="producto">
-        <img class="img" src="/img/productos-png/gin_bombay.png" alt="">
-        <h3 class="nombre"><Bombay><strong>Gin Bombay</strong></Bombay></h3>
-        <p class="precio">$900</p>
-        <p class="precioSinIVA">$738 S/IVA</p>
-        <button class="btn">Agregar Unidad</button><br>
-        <button class="btn">Agregar Caja(12u.)</button>
-    </li> */
+        carrito[indiceProductoExiste].cantidad = nuevaCantidad;
+
+    }
+
+    renderizarCarrito(carrito);
+
+    agregarCarritoALS();
+}
+
+function renderizarTotal() {
+
+    const total = carrito.reduce((acc, productoCarrito) => {
+        return acc + (productoCarrito.precioMasIVA * productoCarrito.cantidad);
+    }, 0);
+    console.log(total);
+    precioFinal.innerHTML = `$${total}`;
+}
+
+function obtenerCarritoLS() {
+
+    let carrito = [];
+    const carritoLS = localStorage.getItem("carrito");
+
+    if (carritoLS !== null) {
+        carrito = JSON.parse(carritoLS);
+    }
+
+    return carrito;
+}
+
+function agregarCarritoALS() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function agregarAlCarrito(producto, cantidad) {
+
+    const productoExiste = carrito.findIndex((productoCarrito) => {
+        return productoCarrito.nombre === producto.nombre;
+    });
+
+    if (productoExiste === -1) {
+
+        carrito.push({
+            nombre: producto.nombre,
+            precio: producto.precio,
+            img: producto.img,
+            precioMasIVA: Math.round(producto.precio * 1.22),
+            cantidad: cantidad
+        })
+
+    } else {
+
+        carrito[productoExiste].cantidad += cantidad;
+    }
+    renderizarCarrito(carrito);
+
+    agregarCarritoALS();
+}
+
+function renderizarCarrito(productos) {
+
+    //limpiarcarrito
+
+    listadoCarrito.innerHTML = "";
+
+    for (const producto of productos) {
+
+        const li = document.createElement("li");
+        li.className = "productoEnCarrito";
+
+        const img = document.createElement("img");
+        img.className = "imgCarrito";
+        img.src = `${producto.img}`;
+        img.alt = "Imagen Producto en Carrito";
+
+        const h6 = document.createElement("h6");
+        h6.innerHTML = `${producto.nombre}`;
+
+        const pPrecio = document.createElement("p");
+        pPrecio.className = "precio"
+        pPrecio.innerHTML = `$${producto.precioMasIVA}`;
+
+        const pSinIva = document.createElement("p");
+        pSinIva.className = "precioSinIVA";
+        pSinIva.innerHTML = `$${producto.precio} S/ IVA`;
+
+        const cantidad = document.createElement("p");
+        cantidad.className = "cantidad";
+        const spanCantidad = document.createElement("span");
+        spanCantidad.innerHTML = `Cantidad: ${producto.cantidad}`;
+
+        cantidad.append(spanCantidad);
+
+        spanCantidad.addEventListener("click", () => {
+
+            spanCantidad.className = "ocultar";
+
+            const inputCantidad = document.createElement("input");
+            inputCantidad.value = producto.cantidad;
+
+            inputCantidad.addEventListener("change", () => {
+
+                const nuevaCantidad = inputCantidad.value;
+
+                spanCantidad.className = "mostrar";
+
+                inputCantidad.remove();
+
+                editarCantidad(producto, nuevaCantidad);
+            });
+
+            cantidad.append(inputCantidad);
+
+        });
+
+        const total = document.createElement("p");
+        total.className = "total";
+        total.innerHTML = `Total: $${producto.precioMasIVA * producto.cantidad}`;
+
+        //agregamos todo al li
+
+        li.append(img, h6, pPrecio, pSinIva, cantidad, total);
+
+        //agregamos a la lista
+
+        listadoCarrito.append(li);
+    }
+    renderizarTotal();
+}
 
 function renderizarProductos(productos) {
 
@@ -36,7 +144,7 @@ function renderizarProductos(productos) {
 
     listadoProductos.innerHTML = "";
 
-    for(const producto of productos) {
+    for (const producto of productos) {
 
         const li = document.createElement("li");
         li.className = "producto";
@@ -52,7 +160,7 @@ function renderizarProductos(productos) {
 
         const pPrecio = document.createElement("p");
         pPrecio.className = "precio"
-        pPrecio.innerHTML = `$${ Math.round(producto.precio * 1.22)}`;
+        pPrecio.innerHTML = `$${Math.round(producto.precio * 1.22)}`;
 
         const pSinIva = document.createElement("p");
         pSinIva.className = "precioSinIVA";
@@ -90,87 +198,22 @@ function renderizarProductos(productos) {
     }
 }
 
-//FUNCION RENDERIZAR CARRITO
+//INICIO DEL PROGRAMA
 
-/* <li class="productoEnCarrito">
-    <img class="imgCarrito" src="/img/productos-png/gin_bombay.png" alt="Imagen Producto en Carrito">
-    <h6>Gin Bombay</h6>
-    <p class="precio">854</p>
-    <p class="precioSinIVA">700</p>
-    <p class="cantidad">5</p>
-    <p class="total">4270</p>
-</li> */
+const carrito = obtenerCarritoLS();
+const listadoProductos = document.getElementById("listadoProductos");
+const listadoCarrito = document.getElementById("listadoCarrito");
+const precioFinal = document.querySelector("#precioFinal span");
 
-function renderizarCarrito(productos) {
+//OBTENEMOS LOS PRODUCTOS JSON
 
-    //limpiarcarrito
-
-    listadoCarrito.innerHTML = "";
-
-    for(const producto of productos) {
-
-        const li = document.createElement("li");
-        li.className = "productoEnCarrito";
-
-        const img = document.createElement("img");
-        img.className = "imgCarrito";
-        img.src = `${producto.img}`;
-        img.alt = "Imagen Producto en Carrito";
-
-        const h6 = document.createElement("h6");
-        h6.innerHTML = `${producto.nombre}`;
-
-        const pPrecio = document.createElement("p");
-        pPrecio.className = "precio"
-        pPrecio.innerHTML = `$${producto.precioMasIVA}`;
-
-        const pSinIva = document.createElement("p");
-        pSinIva.className = "precioSinIVA";
-        pSinIva.innerHTML = `$${producto.precio} S/ IVA`;
-
-        const cantidad = document.createElement("p");
-        cantidad.className = "cantidad";
-        cantidad.innerHTML = `Cantidad: ${producto.cantidad}`;
-
-        const total = document.createElement("p");
-        total.className = "total";
-        total.innerHTML = `Total: $${producto.precioMasIVA * producto.cantidad}`;
-    
-        //agregamos todo al li
-
-        li.append(img, h6, pPrecio, pSinIva, cantidad, total);
-
-        //agregamos a la lista
-
-        listadoCarrito.append(li);
-
-        totalAPagar = producto.precioMasIVA * producto.cantidad ;
-        console.log(totalAPagar)
-    
-    }
-}
-
-//FUNCION AGREGAR AL CARRITO
-
-function agregarAlCarrito(producto, cantidad) {
-
-    const productoExiste = carrito.findIndex( (productoCarrito) => {
-        return productoCarrito.nombre === producto.nombre;
+fetch("/productos.json")
+    .then((response) => {
+        return response.json();
+    })
+    .then((responseProductos) => {
+        renderizarProductos(responseProductos);
     });
 
-    if (productoExiste === -1) {
-
-        carrito.push({
-            nombre: producto.nombre,
-            precio: producto.precio,
-            img: producto.img,
-            precioMasIVA: Math.round(producto.precio * 1.22),
-            cantidad: cantidad
-        })
-
-    } else {
-
-        carrito[productoExiste].cantidad += cantidad;
-    }
-    renderizarCarrito(carrito);
-}
+//RENDERIZAR LOS PRODUCTOS
+renderizarCarrito(carrito);
